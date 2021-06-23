9e0035064e9beefe20968ed77aec5671a8f38b0c
cyishere <cyishere@gmail.com>
2021-06-23 16:25:09 +0800

    init

---
+++ components/SEO.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const SEO = () => {
+  return (
+    <Head>
+      <title>Copycat of Bejamas</title>
+      <meta
+        name="description"
+        content="A copycat of bejamas.io for demonstrating frontend dev."
+      />
+      <link rel="icon" href="/favicon.png" />
+    </Head>
+  );
+};
+
+export default SEO;
---
+++ styles/constants.js
@@ -0,0 +1,15 @@
+export const FONTS = {
+  main: "Poppins, sans-serif",
+  heading: "Recoleta, sans-serif",
+
+  smaller: "0.8125rem",
+  small: "0.875rem",
+  base: "1rem",
+  md: "1.25rem",
+  lg: "1.55rem",
+  xl: "2rem",
+  xxl: "2.25rem",
+  big: "4.45rem",
+};
+
+// 13px, 14px, 16px, 20px, 24.8px, 33.1958px, 36px, 71.2px

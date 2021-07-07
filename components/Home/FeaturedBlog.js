6ad659b4c0367453bde289546450b163e6c58281
cyishere <cyishere@gmail.com>
2021-07-07 17:42:39 +0800

    sticky sidebar

--- components/Home/FeaturedBlog.js
+++ components/Home/FeaturedBlog.js
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 import { Section, SectionHeader } from "../Layout";
 import { Title, SubTitle, Marker } from "../Typography";
 import { ButtonLink } from "../Button";
@@ -11,26 +13,28 @@ const ClientsFrom = () => {
     <Section>
       <OneNTwoColumns>
         <SectionHeader special>
-          <SubTitle>
-            <Marker>Jamstack Blog</Marker>
-          </SubTitle>
-          <Title>
-            Guides.
-            <br />
-            Case Studies.
-            <br />
-            Backstage.
-          </Title>
-          <p>
-            Worth a thousand words.
-            <br />
-            <strong>103,332</strong> to be precise.
-          </p>
-          <FlexStart>
-            <ButtonLink variant="primary" href="/blog">
-              Go to Blog
-            </ButtonLink>
-          </FlexStart>
+          <StickyContainer>
+            <SubTitle>
+              <Marker>Jamstack Blog</Marker>
+            </SubTitle>
+            <Title>
+              Guides.
+              <br />
+              Case Studies.
+              <br />
+              Backstage.
+            </Title>
+            <p>
+              Worth a thousand words.
+              <br />
+              <strong>103,332</strong> to be precise.
+            </p>
+            <FlexStart>
+              <ButtonLink variant="primary" href="/blog">
+                Go to Blog
+              </ButtonLink>
+            </FlexStart>
+          </StickyContainer>
         </SectionHeader>
 
         <TwoColumns break="small">
@@ -42,5 +46,11 @@ const ClientsFrom = () => {
     </Section>
   );
 };
+
+const StickyContainer = styled.div`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 120px;
+`;
 
 export default ClientsFrom;
